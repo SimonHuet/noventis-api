6d94c560f7c01f5250d351054ad1e0c1b27562dd
unknown <simon.huet@epsi.fr>
2019-05-16 10:36:46 +0200

    Add json response

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,40 @@
-fn main() {
+#![feature(proc_macro_hygiene, decl_macro)]
 
- println!("Hello world ! " );
+#[macro_use] extern crate rocket;
+#[macro_use] extern crate rocket_contrib;
+#[macro_use] extern crate serde_derive;
+
+use rocket_contrib::{ Json ,value};
+
+mod user;
+use user::{User};
+
+#[post("/", data = "<user>")]
+fn create(user: Json<User>) -> Json<User> {
+    user
+}
+
+#[get("/")]
+fn read() -> Json<Value> {
+    Json(json!([
+        "User 1", 
+        "User 2"
+    ]))
+}
+/*
+#[put("/<id>", data = "<user>")]
+fn update(id: i32, user: Json<User>) -> Json<User> {
+    user
+}
+
+#[delete("/<id>")]
+fn delete(id: i32) -> Json<Value> {
+    Json(json!({"status": "ok"}))
+}*/
+
+fn main() {
+    rocket::ignite()
+        .mount("/user", routes![create /*,update, delete*/])
+        .mount("/users", routes![read])
+        .launch();
 }
